a29cf4cfa9829ac1749979adef0b001e8ceba8eb
Supakit <supakit.kitj@gmail.com>
2025-06-10 00:32:16 +0700

    update docker

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,13 +3,13 @@ import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class UserController {
-
   @MessagePattern({ cmd: 'get_users' })
-  @Get('/users')
   getUsers() {
+
     return [
       { id: 1, name: 'Alice' },
       { id: 2, name: 'Bob' },
+      { id: 3, name: 'KKTEST' },
     ];
   }
 }
